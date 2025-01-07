a9d8f5a2e21634fda48510917ee79d242571fc8f
Leif Eriksen <leif.eriksen.au@gmail.com>
2025-01-08 00:19:45 +1100

    fix kv processing

--- json/parse.ps1
+++ json/parse.ps1
@@ -1,13 +1,9 @@
 function Set-KeyVault {
   param (
-    $subscription,
-    $resourceGroup,
     $kvName
   )
 
   $value = [PSCustomObject]@{
-    "subscription"  = $subscription;
-    "resourceGroup" = $resourceGroup;
     "kvName"        = $kvName
   }
 
@@ -35,13 +31,11 @@ function Set-k8sCluster {
   return $value
 }
 
-Write-Host $(Get-Location)
-
 $myJson = Get-Content .\test.json -Raw | ConvertFrom-Json
 
 ForEach ($config in $myJson) {
   if ($config.target -eq "akv") {
-    Set-KeyVault   -subscription $config.target_sub -resourceGroup $config.target_rg -kvName $config.target_name
+    Set-KeyVault   -kvName $config.target_name
   } elseif ($config.target -eq "k8s") {
     Set-k8sCluster -subscription $config.target_sub -resourceGroup $config.target_rg -clusterName $config.target_name
   }
