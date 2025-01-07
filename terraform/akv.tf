30a5b84524f9625590b068a22881b6233f1552f4
Leif Eriksen <leif.eriksen.au@gmail.com>
2025-01-07 21:43:23 +1100

    add kv tags

--- terraform/akv.tf
+++ terraform/akv.tf
@@ -6,4 +6,5 @@ resource azurerm_key_vault akv {
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
+  tags                       = module.environment.tags
 }
